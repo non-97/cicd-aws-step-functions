4bd6ff31dc98b5148401d228bbec249bd0f80b35
non-97 <nonmonaka07@gmail.com>
2022-01-26 15:21:10 +0900

    Changed to pass the IAM Policy Document and add its permissions to the IAM Role.

--- src/codeBuild/buildCommand.sh
+++ src/codeBuild/buildCommand.sh
@@ -9,11 +9,11 @@ stack_name="$3"
 state_machine_name="$4"
 stack_id_after_stack_name="$5"
 
-echo STATE_MACHINE_IAM_ROLE : $STATE_MACHINE_IAM_ROLE
 echo CRON : "'$CRON'"
 echo EVENT_PATTERN : "'$EVENT_PATTERN'"
 echo TARGET_EVENT_BUS_ARN_AFTER_EXECUTION : "'$TARGET_EVENT_BUS_ARN_AFTER_EXECUTION'"
-echo IS_ENABLED_TRACING : "'$IS_ENABLED_TRACING'"
+echo XRAY_TRACING : "'$XRAY_TRACING'"
+echo IAM_POLICY_DOCUMENT : $IAM_POLICY_DOCUMENT
 
 cd sam-sfn
 
@@ -36,7 +36,7 @@ if [ -s ./statemachine/StateMachineWorkFlow.asl.json ]; then
     --stack-name $state_machine_name \
     --capabilities CAPABILITY_IAM \
     --no-fail-on-empty-changeset \
-    --parameter-overrides StateMachineName=$state_machine_name StackIdAfterStackName=$stack_id_after_stack_name Cron="'$CRON'" EventPattern="'$EVENT_PATTERN'" TargetEventBusArnAfterExecution="'$TARGET_EVENT_BUS_ARN_AFTER_EXECUTION'" IsEnabledTracing="'$IS_ENABLED_TRACING'"
+    --parameter-overrides StateMachineName=$state_machine_name StackIdAfterStackName=$stack_id_after_stack_name Cron="'$CRON'" EventPattern="'$EVENT_PATTERN'" TargetEventBusArnAfterExecution="'$TARGET_EVENT_BUS_ARN_AFTER_EXECUTION'" XRayTracing="'$XRAY_TRACING'" IamPolicyDocument="'$IAM_POLICY_DOCUMENT'"
 
   aws cloudformation describe-stacks --stack-name $state_machine_name
 else
--- src/codeBuild/preBuildCommand.sh
+++ src/codeBuild/preBuildCommand.sh
@@ -7,20 +7,21 @@ bucket_name="$1"
 sam_file_name="$2"
 
 
-STATE_MACHINE_IAM_ROLE=`yq -r '.Settings.iam_role_arn' StateMachineSettings.yml`
-echo STATE_MACHINE_IAM_ROLE : $STATE_MACHINE_IAM_ROLE
-
 CRON=`yq -r '.Settings.cron' StateMachineSettings.yml`
 echo CRON : "'$CRON'"
 
 EVENT_PATTERN=`yq -r '.Settings.event_pattern' StateMachineSettings.yml`
-echo EVENT_PATTERN : "'EVENT_PATTERN'"
+echo EVENT_PATTERN : "'$EVENT_PATTERN'"
 
 TARGET_EVENT_BUS_ARN_AFTER_EXECUTION=`yq -r '.Settings.target_event_bus_arn_after_execution' StateMachineSettings.yml`
-echo TARGET_EVENT_BUS_ARN_AFTER_EXECUTION : "'TARGET_EVENT_BUS_ARN_AFTER_EXECUTION'"
+echo TARGET_EVENT_BUS_ARN_AFTER_EXECUTION : "'$TARGET_EVENT_BUS_ARN_AFTER_EXECUTION'"
 
-IS_ENABLED_TRACING=`yq -r '.Settings.is_enabled_tracing' StateMachineSettings.yml`
-echo IS_ENABLED_TRACING : "'IS_ENABLED_TRACING'"
+XRAY_TRACING=`yq -r '.Settings.xray_tracing' StateMachineSettings.yml`
+echo XRAY_TRACING : "'$XRAY_TRACING'"
+
+IAM_POLICY_DOCUMENT=`yq -r '.Settings.iam_policy_document' StateMachineSettings.yml`
+echo IAM_POLICY_DOCUMENT : "'$IAM_POLICY_DOCUMENT'"
+
 
 # Download the AWS SAM template file from the S3 bucket
 aws s3 cp s3://$bucket_name/$sam_file_name $sam_file_name
