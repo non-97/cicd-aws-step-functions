88f3fdc7441c9a6c6066ccb7f3c613b6e82e8a0c
non-97 <nonmonaka07@gmail.com>
2022-03-07 17:33:34 +0900

    Added comments and changed variable names.

--- src/codebuild/build_command.sh
+++ src/codebuild/build_command.sh
@@ -8,6 +8,7 @@ sam_file_name="$2"
 state_machine_name="$3"
 stack_unique_id="$4"
 
+# Check variables
 echo deployment_destination_account_iam_role_arn : ${deployment_destination_account_iam_role_arn}
 
 IFS=';'; cron_array=(${tmp_cron_array}); unset IFS
@@ -24,10 +25,13 @@ echo xray_tracing : ${xray_tracing}
 echo iam_policy_document : ${iam_policy_document}
 echo tags_list : ${tags_list}
 
+# Change to the directory where AWS SAM CLI is to be executed
 cd sam-sfn
 
+# Check that the State Machine workflow exists
 ls -l ./state_machine/StateMachineWorkFlow.asl.json
 
+# Assume Role if the deployment destination is a different AWS account
 if [[ "${deployment_destination_account_iam_role_arn}" != null ]]; then
   before=$(aws sts get-caller-identity | jq -r .Arn)
   output=$(aws sts assume-role --role-arn ${deployment_destination_account_iam_role_arn} --role-session-name sam-deploy-session)
@@ -41,6 +45,7 @@ fi
 
 AWS_ACCOUNT=$(aws sts get-caller-identity | jq -r .Account)
 
+# If the "StateMachineWorkFlow.asl.json" is not empty, then Deploying resources with AWS SAM CLI
 if [[ -s ./state_machine/StateMachineWorkFlow.asl.json ]]; then
   sam build \
     --template-file ${sam_file_name}
@@ -68,15 +73,18 @@ if [[ -s ./state_machine/StateMachineWorkFlow.asl.json ]]; then
         XRayTracing=${xray_tracing} \
         IamPolicyDocument="'${iam_policy_document}'")
 
+  # If tags are specified, add an option to specify tags
   if [[ -n "${tags_list}" ]]; then
     "${deploy_command[@]}" \
       --tags "'${tags_list}'"
   else
     "${deploy_command[@]}"
   fi
+
+  # After deployment, the stack is displayed
   aws cloudformation describe-stacks --stack-name ${state_machine_name}
 else
-  # If the ASL file is empty, delete the stack
+  # If the "StateMachineWorkFlow.asl.json" is empty, delete the stack
   yes | \
   sam delete \
     --stack-name ${state_machine_name}
